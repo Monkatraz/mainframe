4e7098a70760344dc869e733e37d0ccc1791603a
Monkatraz <34875062+Monkatraz@users.noreply.github.com>
2020-10-26 17:05:42 -0600

    Revamped gestures for touch support and compactness

--- src/static/js/modules/gestures.ts
+++ src/static/js/modules/gestures.ts
@@ -10,19 +10,20 @@ type Point = [x: number, y: number]
 type SwipeDirection = 'up' | 'down' | 'left' | 'right'
 type SwipeEvent = [sig: SwipeDirection, dist: number]
 
-function calcSwipe(start: Point, end: Point): SwipeEvent {
-  const coord = [start, end]
+// Constants
+const SWIPE_DIRECTIONS: SwipeDirection[] = ['up', 'down', 'left', 'right']
+
+function resolveSwipe([x1, y1]: Point, [x2, y2]: Point): SwipeEvent {
   // index 0 is vertical diff, index 1 is horizontal diff
-  const diff = [coord[0][1] - coord[1][1], coord[0][0] - coord[1][0]]
-  const diffAbs = [Math.abs(diff[0]), Math.abs(diff[1])]
+  const diff = [y1 - y2, x1 - x2]
+  const diffAbs = diff.map(Math.abs)
 
   // 0 is vertical, 1 is horizontal
   const axis = diffAbs[1] > diffAbs[0] ? 1 : 0
   // 0 is up / left, 1 is down / right
   const dir = diff[axis] > 0 ? 0 : 1
 
-  const dirs = ['up', 'down', 'left', 'right']
-  const swipeDir = dirs[(axis * 2) + dir] as SwipeDirection
+  const swipeDir = SWIPE_DIRECTIONS[(axis * 2) + dir]
   //                      ^ this is either 0 or 2, as axis is either 0 or 1
 
   return [swipeDir, diffAbs[axis]]
@@ -32,6 +33,8 @@ function calcSwipe(start: Point, end: Point): SwipeEvent {
 
 interface SwipeGestureOpts {
   condition: () => boolean
+  useTouchEvents: boolean
+  setTouchAction: boolean,
   immediate: boolean
   startThreshold: number
   endThreshold: number
@@ -40,6 +43,8 @@ interface SwipeGestureOpts {
 
 const SWIPE_GESTURE_DEFAULT_OPTS = {
   condition: () => true,
+  useTouchEvents: true,
+  setTouchAction: true,
   immediate: true,
   startThreshold: 10,
   endThreshold: 35,
@@ -54,85 +59,86 @@ export function SwipeGesture(
   userOpts: Partial<SwipeGestureOpts> = {}
 ) {
   const opts = { ...SWIPE_GESTURE_DEFAULT_OPTS, ...userOpts } as SwipeGestureOpts
-
-  target.style.touchAction = 'none'
-
   const evtOpts = { passive: true }
-  const events = ['pointermove', 'pointerup', 'pointercancel']
 
-  let start: Point
+  if (opts.setTouchAction) target.style.touchAction = 'none'
+
+  const events = opts.useTouchEvents
+    ? ['touchstart', 'touchmove', 'touchend', 'touchcancel']
+    : ['pointerdown', 'pointermove', 'pointerup', 'pointercancel']
+
   let ID: number
+  let start: Point
   let current: SwipeEvent
-  let timeout: number
-  let timedout: boolean | null
+  let timeout: number | undefined
 
-  const handler = ((evt: PointerEvent) => {
-    // Return if it's a different pointer (except if it's new)
-    if (evt.type !== 'pointerdown' && evt.pointerId !== ID) return
-    if (!evt.isPrimary) return
-
-    const cleanup = () => {
-      rmEvtlistener(document, events, handler)
+  const setState = (val: boolean) => {
+    if (val) {
+      evtlistener(document, events.slice(1), handler, evtOpts)
+      current = ['up', 0]
+      timeout = undefined
+    }
+    else {
+      rmEvtlistener(document, events.slice(1), handler)
       clearInterval(timeout)
     }
-
-    switch (evt.type) {
-      case 'pointerdown': {
-        if (!opts.condition()) return
-        // Assign functions
-        // Attach to document so that we don't have to pointer capture
-        evtlistener(document, events, handler, evtOpts)
-        // Starting vars
-        start = [evt.clientX, evt.clientY]
-        ID = evt.pointerId
-        current = calcSwipe(start, start)
-        timedout = null
-        break
-      }
-
-      case 'pointermove': {
-        current = calcSwipe(start, [evt.clientX, evt.clientY])
-
-        if (timedout === null && current[1] > opts.startThreshold) {
-          timedout = false
-          timeout = setTimeout(() => {
-            timedout = true
-          }, opts.timeout)
-        }
-
-        if (opts.immediate && !timedout) {
-          // Correct direction and greater than our threshold?
-          if (current[0] === dir && current[1] > opts.endThreshold) {
-            cleanup()
-            fn()
-          }
-        }
-        break
-      }
-
-      case 'pointerup': {
-        cleanup()
-        if (timedout) return
-
-        // Correct direction and greater than our threshold?
-        if (current[0] === dir && current[1] > opts.endThreshold) fn()
-        break
-      }
-
-      case 'pointercancel': {
-        cleanup()
-        break
-      }
-    }
-  })
-
-  target.addEventListener('pointerdown', handler, evtOpts)
-
-  const destroy = () => {
-    rmEvtlistener(document, events, handler)
-    clearInterval(timeout)
-    target.removeEventListener('pointerdown', handler)
   }
 
+  const eventFns: ((evt: PointerEvent | TouchEvent) => void)[] = [
+    // Start
+    (evt) => {
+      if (!opts.condition()) return
+      setState(true)
+      if (evt instanceof PointerEvent) {
+        ID = evt.pointerId
+        start = [evt.clientX, evt.clientY]
+      } else {
+        const touch = evt.changedTouches[0]
+        ID = touch.identifier
+        start = [touch.clientX, touch.clientY]
+      }
+    },
+    // Move
+    (evt) => {
+      if (evt instanceof PointerEvent) {
+        if (evt.pointerId !== ID) return
+        current = resolveSwipe(start, [evt.clientX, evt.clientY])
+      } else {
+        const touch = [...evt.changedTouches].find((val) => val.identifier === ID)
+        if (!touch) return
+        current = resolveSwipe(start, [touch.clientX, touch.clientY])
+      }
+
+      if (!timeout && current[1] > opts.startThreshold) {
+        setTimeout(() => { setState(false) }, opts.timeout)
+      }
+      if (opts.immediate && current[0] === dir && current[1] > opts.endThreshold) {
+        setState(false)
+        fn()
+      }
+    },
+    // Up
+    (evt) => {
+      setState(false)
+      if (current[0] === dir && current[1] > opts.endThreshold) fn()
+    },
+    // Cancel
+    (evt) => {
+      setState(false)
+    }
+  ]
+
+  const handler = (evt: PointerEvent | TouchEvent) => {
+    const index = events.indexOf(evt.type)
+    if (index !== -1) eventFns[index](evt)
+  }
+
+  target.addEventListener(events[0] as 'pointerdown', handler, evtOpts)
+
+  const destroy = () => {
+    setState(false)
+    target.removeEventListener(events[0] as 'pointerdown', handler)
+  }
+  // TODO: update props func.
   return { handler, destroy }
 }
