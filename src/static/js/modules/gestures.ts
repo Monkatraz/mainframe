6248da2ad8f2948dc8ddb4e9337b361be5f69926
Monkatraz <34875062+Monkatraz@users.noreply.github.com>
2020-10-26 19:03:51 -0600

    evEN MORE COMPACT

--- src/static/js/modules/gestures.ts
+++ src/static/js/modules/gestures.ts
@@ -42,7 +42,7 @@ const SWIPE_GESTURE_DEFAULT_OPTS: SwipeGestureOpts = {
   endThreshold: 35,
   timeout: 250
 }
-
+// TODO: Callback thingy for getting the current dist value
 export function swipeGesture(target: HTMLElement, inOpts: Partial<SwipeGestureOpts> = {}) {
   let opts: SwipeGestureOpts
   const update = (newOpts: Partial<SwipeGestureOpts>) => {
@@ -50,55 +50,39 @@ export function swipeGesture(target: HTMLElement, inOpts: Partial<SwipeGestureOp
     target.style.touchAction = opts.setTouchAction ? 'none' : ''
   }
   const destroy = () => {
-    setState(false)
+    disable()
     target.removeEventListener('pointerdown', handler)
   }
 
   update(inOpts)
-  const events = ['pointermove', 'pointerup', 'pointercancel']
   let ID = -1
   let start: Point
-  let current: SwipeEvent
   let timeout: number | undefined = undefined
 
-  const setState = (val: boolean) => {
+  const disable = () => {
     clearInterval(timeout)
-    current = ['up', 0]
     timeout = undefined
     ID = -1
-    if (val) evtlistener(document, events, handler, { passive: true })
-    else rmEvtlistener(document, events, handler)
-  }
-
-  const eventFns: { [evtType: string]: (evt: PointerEvent) => void } = {
-    'pointerdown': (evt) => {
-      if (!opts.condition()) return
-      setState(true)
-      ID = evt.pointerId
-      start = [evt.clientX, evt.clientY]
-    },
-    'pointermove': (evt) => {
-      current = resolveSwipe(start, [evt.clientX, evt.clientY])
-      if (!timeout && current[1] > opts.startThreshold) {
-        setTimeout(() => { setState(false) }, opts.timeout)
-      }
-      if (opts.immediate && current[0] === opts.direction && current[1] > opts.endThreshold) {
-        setState(false)
-        opts.do()
-      }
-    },
-    'pointerup': (evt) => {
-      setState(false)
-      if (current[0] === opts.direction && current[1] > opts.endThreshold) opts.do()
-    },
-    'pointercancel': (evt) => {
-      setState(false)
-    }
+    rmEvtlistener(document, ['pointermove', 'pointerup', 'pointercancel'], handler)
   }
 
   const handler = (evt: PointerEvent) => {
     if (ID !== -1 && ID !== evt.pointerId) return
-    eventFns[evt.type](evt)
+    if (evt.type === 'pointerdown' && opts.condition()) {
+      evtlistener(document, ['pointermove', 'pointerup', 'pointercancel'], handler, { passive: true })
+      ID = evt.pointerId
+      start = [evt.clientX, evt.clientY]
+    }
+    else if (evt.type !== 'pointerdown') {
+      const current = resolveSwipe(start, [evt.clientX, evt.clientY])
+      const swipeValid = current[0] === opts.direction && current[1] > opts.endThreshold
+      if (evt.type === 'pointermove') {
+        if (!timeout && current[1] > opts.startThreshold) { setTimeout(() => { disable() }, opts.timeout) }
+        if (opts.immediate && swipeValid) { disable(), opts.do() }
+      }
+      else if (evt.type === 'pointerup' && swipeValid) opts.do()
+      if (evt.type === 'pointercancel' || evt.type === 'pointerup') disable()
+    }
   }
   target.addEventListener('pointerdown', handler, { passive: true })
 
