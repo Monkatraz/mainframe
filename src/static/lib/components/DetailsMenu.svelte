211dac5d3bcb883c84db68dff68f25b27e0b7580
Monkatraz <34875062+Monkatraz@users.noreply.github.com>
2021-01-13 17:50:50 -0700

    Added hover option for details menu

--- src/static/lib/components/DetailsMenu.svelte
+++ src/static/lib/components/DetailsMenu.svelte
@@ -1,10 +1,13 @@
 <script lang='ts'>
+  import { onMount } from 'svelte';
   import Icon from './Icon.svelte'
 
   export let i = ''
   export let size = '1.5rem'
+  export let hover = false
 
   let details: HTMLElement
+  let summary: HTMLElement
 
   function checkClose(evt: PointerEvent) {
     if (!details.hasAttribute('open')) return
@@ -14,6 +17,17 @@
     // all checks failed
     details.removeAttribute('open')
   }
+
+  onMount(() => {
+    if (hover) {
+      summary.addEventListener('pointerover', () => {
+        details.setAttribute('open', '')
+      })
+      details.addEventListener('pointerleave', () => {
+        details.removeAttribute('open')
+      })
+    }
+  })
 
 </script>
 
@@ -78,7 +92,7 @@
 <svelte:body on:pointerdown={checkClose} />
 
 <details bind:this={details} {...$$restProps}>
-  <summary><Icon {i} {size}/></summary>
+  <summary bind:this={summary}><Icon {i} {size}/></summary>
   <div class=menu>
     <svg class='arrow' xmlns='http://www.w3.org/2000/svg' aria-hidden='true' focusable='false' width='1rem' height='1rem' viewBox='0 0 24 24'><path d='M3 19h18a1.002 1.002 0 0 0 .823-1.569l-9-13c-.373-.539-1.271-.539-1.645 0l-9 13A.999.999 0 0 0 3 19z'/></svg>
     <slot />
